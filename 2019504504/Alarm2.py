e93693237d84fbc42616e1e863238bca9cdf6729
aditya0028halo <64909501+aditya0028halo@users.noreply.github.com>
2022-10-15 12:23:57 +0530

    Delete 2019504504 directory

--- 2019504504/Alarm2.py
+++
@@ -1,7 +0,0 @@
-import random
-tt=50
-while(1):
-    temp=random.randrange(10,75)
-    hum=random.randrange(0,80)
-    al = "On" if tt < temp else "Off"
-    print("Temperature: ",temp," Alarm: ",al)
